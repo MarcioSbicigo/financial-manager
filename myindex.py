f68154489337c79082941d3542099f3b5988bc92
Marcio Sbicigo <marciosbicigo@hotmail.com>
2023-03-21 15:21:10 -0300

    First commit - Basic structure

---
+++ myindex.py
@@ -0,0 +1,23 @@
+from dash import html, dcc
+import dash
+from dash.dependencies import Input, Output
+import dash_bootstrap_components as dbc
+import pandas as pd
+import plotly.express as px
+from app import *
+
+# ========== INÍCIO LAYOUT ========== #
+
+content = html.Div(id="page-content")
+
+
+app.layout = dbc.Container(children=[
+
+
+
+
+], fluid=True,)
+
+
+if __name__ == '__main__':
+    app.run_server(port=8051, debug=True)
